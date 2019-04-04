07656e84fedea63b65a9ffe6d1c37305cf843a00
Harikrishnan V K <42973680+vkhari93@users.noreply.github.com>
2019-04-04 23:56:39 +0530

    Add files via upload

---
+++ knn.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
+dataset = r'H:\Data\Social_Network_Ads.csv'
+data = pd.read_csv(dataset)
+
+train_data = data.sample(frac=0.8, random_state=0)
+test_data = data.drop(train_data.index)
+
+train_features = train_data.iloc[:, 2:4]
+train_label = train_data.iloc[:, 4]
+
+test_features = test_data.iloc[:, 2:4]
+test_label = test_data.iloc[:, 4]
+
+scaler = StandardScaler()
+train_features = scaler.fit_transform(train_features)
+test_features = scaler.transform(test_features)
+
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(train_features, train_label)
+
+predict_data = classifier.predict(test_features)
+
+# confusion matrix
+matrix = confusion_matrix(test_label, predict_data)
+print("Confusion matrix for Decision Tree prediction \n", matrix)
+
+# accuracy
+accuracy = (matrix[0, 0]+matrix[1, 1])/matrix.sum()
+print("Decision Tree prediction accuracy is ", accuracy)
